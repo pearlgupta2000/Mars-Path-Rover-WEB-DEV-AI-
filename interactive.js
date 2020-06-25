226a4f81ec40cec2bef27bacfed3d514c3abf218
pearl gupta <pearlgupta2000@gmail.com>
2020-06-25 22:46:18 +0530

    Front End

--- interactive.js
+++ interactive.js
@@ -9,5 +9,78 @@ function createGrid() {
     $(".grid").height(34.55);
 };
 
+var id=1;
 
+function close(){
+	if(id==1){
+		document.getElementById("mydrop").classList.toggle("show");	
+	}
+	else if(id==2){
+		document.getElementById("mydrop2").classList.toggle("show");	
+	}
+	else if(id==3){
+		document.getElementById("mydrop3").classList.toggle("show");	
+	}
+	else if(id==4){
+		document.getElementById("mydrop4").classList.toggle("show");	
+	}
+	else if(id==5){
+		document.getElementById("mydrop5").classList.toggle("show");	
+	}
+	else if(id==6){
+		document.getElementById("mydrop6").classList.toggle("show");	
+	}
+	else if(id==7){
+		document.getElementById("mydrop7").classList.toggle("show");	
+	}
+	else if(id==8){
+		document.getElementById("mydrop8").classList.toggle("show");	
+	}
+	else{
+		return;
+	}
+}
+
+function A_algo(){
+	close();
+document.getElementById("mydrop").classList.toggle("show");	
+id=1;
+}	
+function IDA_algo(){
+	close();
+	document.getElementById("mydrop2").classList.toggle("show");
+	id=2;
+}
+function BFS_algo(){
+	close();
+	document.getElementById("mydrop3").classList.toggle("show");
+	id=3;
+}
+function BtFS_algo(){
+	close();
+	document.getElementById("mydrop4").classList.toggle("show");
+	id=4;
+}
+
+function DJK_algo(){
+	close();
+	document.getElementById("mydrop5").classList.toggle("show");
+	id=5;
+}
+function JPS_algo(){
+	close();
+	document.getElementById("mydrop6").classList.toggle("show");
+	id=6;
+}
+
+function OJPS_algo(){
+	close();
+	document.getElementById("mydrop7").classList.toggle("show");
+	id=7;
+}
+function T_algo(){
+	close();
+	document.getElementById("mydrop8").classList.toggle("show");
+	id=8;
+}
 
